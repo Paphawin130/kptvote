10fc190d8d62d0b730a71567a5cedd26217bdc65
Tanny <69077943+Paphawin130@users.noreply.github.com>
2023-11-12 14:05:24 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,21 @@
+// "use server";
 import Image from "next/image";
 import axios from "axios";
+import { TypeAnimation } from "react-type-animation";
+import AnimatedNumber from "react-animated-number";
 
 function Home(props) {
-  const Users = 8
-  const Items = 8
-  const Topups = 8
-  const Historys = 8
+  const Users = "5";
+  const Items = "10,000";
+  const Topups = "1,000";
+  const Historys = "12:03:32";
   return (
-    <main className="w-full container text-center min-h-screen text-purple-400 ">
+    <main className="w-full container text-center min-h-screen text-black-400 ">
       <div data-aos="fade-down" data-aos-duration="800">
-        <h1 class="text-5xl w-fit font-bold text-purple-400 -skew-x-12 tracking-tighter">
-          Kornpitack eSports
+        <h1 className="text-5xl w-fit font-bold text-pink-400 -skew-x-12 tracking-tighter">
+          Kornpitack Esports
         </h1>
       </div>
-      {/* <h1 className="md:w-1/2 text-4xl sm:text-5xl md:text-6xl font-bold text-transparent bg-clip-text bg-gradient-to-t from-blue-500 to-blue-300 nsys_fadeIn mb-6 md:mb-0"></h1> */}
       <div
         className="grid pt-3 d-flex justify-content-center"
         data-aos="zoom-in"
@@ -36,72 +38,177 @@ function Home(props) {
         <div
           className="flex rounded-xl overflow-hidden shadow-lg text-sm sm:text-base"
           data-aos="fade-down"
-        >
-          {/* <p class="bg-gradient-to-tr from-purple-500 to-purple-300 px-4 py-2 text-white whitespace-nowrap" data-aos="fade-down">
-          <span class="hidden sm:inline-flex" data-aos="fade-down">ประกาศ</span>ข่าวสาร
-        </p> */}
-          <marquee class="bg-white py-2">
-            หากพบปัญหา สามารถแจ้งเข้ามาได้ที่ชมรม Coding
-          </marquee>
-        </div>
+        ></div>
       </div>
-
-      <div className="grid grid-cols-1 lg:grid-cols-4 gap-8 gap-x-4 text-blue-500 justify-center text-center pt-7 px-4">
+      <div>
+        <TypeAnimation
+          className="text-4xl font-bold text-black"
+          sequence={["รายละเอียดการโหวต", 3000]}
+          cursor={true}
+          repeat={Infinity}
+        />
+        <hr
+          className="w-48 h-1 mx-auto my-4 bg-gray-100 border-0 rounded md:my-10 dark:bg-gray-700"
+          data-aos="fade-down"
+        ></hr>
+      </div>
+      <div className="grid grid-cols-1 lg:grid-cols-4 gap-8 gap-x-4 text-black-500 justify-center text-center pt-7 px-4">
         <div
-          className="rounded-2xl shadow-xl border-8 bg-white border-white-500 p-6 transition ease-in-out delay-50 hover:-translate-y-7 hover:scale-11 duration-300 cursor-pointer"
+          className="rounded-2xl shadow-xl bg-white border-white-500 p-6 transition ease-in-out delay-50 hover:-translate-y-7 hover:scale-11 duration-300 cursor-pointer"
           data-aos="fade-right"
           data-aos-duration="800"
         >
-          <div className="d-flex justify-content-center">
-            <Image src="/imgs/user.gif" width="60" height="60" alt="user" />
-          </div>
-          <h1 className="text-2xl">{Users}</h1>
-          <h1 className="text-xl">จำนวนผู้ Vote ทั้งหมด</h1>
+          <h1 className="d-flex text-2xl">มีให้เกมโหวตทั้งหมด...</h1>
+          <hr className="w-48 md:my-2"></hr>
+          <br></br>
+          <h1 className="d-flex text-5xl">{Users} เกม</h1>
         </div>
         <div
-          className="rounded-2xl shadow-xl border-8 bg-white border-white-500 p-6 transition ease-in-out delay-50 hover:-translate-y-7 hover:scale-11 duration-300 cursor-pointer"
+          className="rounded-2xl shadow-xl  bg-white border-white-500 p-6 transition ease-in-out delay-50 hover:-translate-y-7 hover:scale-11 duration-300 cursor-pointer"
           data-aos="fade-up"
           data-aos-duration="800"
         >
-          <div className="d-flex justify-content-center">
-            <Image
-              src="/imgs/shopping-cart.gif"
-              width="50"
-              height="50"
-              alt="shopping-cart"
-            />
-          </div>
-          <h1 className="text-2xl">{Items}</h1>
-          <h1 className="text-xl">เกมทั้งหมด</h1>
+          <h1 className="d-flex text-2xl">โหวตไปแล้วทั้งหมด...</h1>
+          <hr className="w-48 md:my-2"></hr>
+          <br></br>
+          <h1 className="d-flex text-5xl">{Items} คน</h1>
         </div>
         <div
-          className="rounded-2xl shadow-xl border-8 bg-white border-white-500 p-6 transition ease-in-out delay-50 hover:-translate-y-7 hover:scale-11 duration-300 cursor-pointer"
+          className="rounded-2xl shadow-xl  bg-white border-white-500 p-6 transition ease-in-out delay-50 hover:-translate-y-7 hover:scale-11 duration-300 cursor-pointer"
           data-aos="fade-down"
           data-aos-duration="800"
         >
-          <div className="d-flex justify-content-center">
-            <Image src="/imgs/dollar.gif" width="50" height="50" alt="dollar" />
-          </div>
-          <h1 className="text-2xl">{Topups}</h1>
-          <h1 className="text-xl">ยอดการ Vote ทั้งหมด</h1>
+          <h1 className="d-flex text-2xl">ยังไม่โหวตทั้งหมด...</h1>
+          <hr className="w-48 md:my-2"></hr>
+          <br></br>
+          <h1 className="d-flex text-5xl">{Topups} คน</h1>
         </div>
         <div
-          className="rounded-2xl shadow-xl border-8 bg-white border-white-500 p-6 transition ease-in-out delay-50 hover:-translate-y-7 hover:scale-11 duration-300 cursor-pointer"
+          className="rounded-2xl shadow-xl bg-white border-white-500 p-6 transition ease-in-out delay-50 hover:-translate-y-7 hover:scale-11 duration-300 cursor-pointer"
           data-aos="fade-left"
           data-aos-duration="800"
         >
-          <div className="d-flex justify-content-center">
-            <Image
-              src="/imgs/money-flow.gif"
-              width="50"
-              height="50"
-              alt="money-flow"
-            />
-          </div>
-          <h1 className="text-2xl">{Historys}</h1>
-          <h1 className="text-xl">จำนวนผู้ลงทะเบียนทั้งหมด</h1>
+          <h1 className="d-flex text-2xl">ปิดโหวตในอีก...</h1>
+          <hr className="w-48 md:my-2"></hr>
+          <br></br>
+          <h1 className="d-flex text-5xl">{Historys}</h1>
         </div>
       </div>
+      <br></br>
+      <br></br>
+      <div>
+        <h1 className="text-4xl font-bold text-black">รายละเอียดเกมทั้งหมด</h1>
+        <hr
+          className="w-48 h-1 mx-auto my-4 bg-gray-100 border-0 rounded md:my-10 dark:bg-gray-700"
+          data-aos="fade-down"
+        ></hr>
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 gap-x-4 text-black-500 justify-center text-center pt-7 px-5">
+          <div
+            className="rounded-2xl shadow-xl bg-white border-white-500 p-6 transition ease-in-out delay-50 hover:-translate-y-7 hover:scale-11 duration-300 cursor-pointer"
+            data-aos="fade-left"
+            data-aos-duration="800"
+          >
+            <div
+              className="grid pt-3  justify-content-center"
+              data-aos="zoom-in"
+            >
+              <img
+                class="img-fluid rounded-top-lg object-cover" // w-[400px] h-[200px]
+                src="https://images.alphacoders.com/128/thumb-1920-1282982.jpg"
+                alt="card_image"
+                data-aos="fade-up"
+                width="1000"
+                height="1000"
+              ></img>
+            </div>
+            <br></br>
+            <br></br>
+            <h1
+              className="text-3xl font-bold text-black"
+              data-aos="fade-down"
+            >
+              Valorant&nbsp;{Topups} คน
+            </h1>
+          </div>
+          <div
+            className="rounded-2xl shadow-xl  bg-white border-white-500 p-6 transition ease-in-out delay-50 hover:-translate-y-7 hover:scale-11 duration-300 cursor-pointer"
+            data-aos="fade-up"
+            data-aos-duration="800"
+          >
+            <img
+              class="img-fluid rounded-top-lg object-cover" // w-[400px] h-[200px]
+              src="https://images.alphacoders.com/128/thumb-1920-1282982.jpg"
+              alt="card_image"
+              data-aos="fade-up"
+              width="1000"
+              height="1000"
+            ></img>
+            <br></br>
+            <br></br>
+            <h1
+              className="text-3xl font-bold text-black"
+              data-aos="fade-down"
+            >
+              Valorant&nbsp;{Topups} คน
+            </h1>
+          </div>
+        </div>
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 gap-x-4 text-black-500 justify-center text-center pt-7 px-5">
+          <div
+            className="rounded-2xl shadow-xl bg-white border-white-500 p-6 transition ease-in-out delay-50 hover:-translate-y-7 hover:scale-11 duration-300 cursor-pointer"
+            data-aos="fade-left"
+            data-aos-duration="800"
+          >
+            <div
+              className="grid pt-3 justify-content-center"
+              data-aos="zoom-in"
+            >
+              <img
+                class="img-fluid rounded-top-lg  object-cover"
+                src="https://images.alphacoders.com/128/thumb-1920-1282982.jpg"
+                alt="card_image"
+                data-aos="fade-up"
+                width="1000"
+                height="1000"
+              ></img>
+            </div>
+            <br></br>
+            <br></br>
+            <h1
+              className=" text-3xl font-bold text-black"
+              data-aos="fade-down"
+            >
+              Overwatch 2&nbsp;{Topups} คน
+            </h1>
+          </div>
+          <div
+            className="rounded-2xl shadow-xl  bg-white border-white-500 p-6 transition ease-in-out delay-50 hover:-translate-y-7 hover:scale-11 duration-300 cursor-pointer"
+            data-aos="fade-up"
+            data-aos-duration="800"
+          >
+            <img
+              class="img-fluid rounded-top-lg object-cover" // w-[400px] h-[200px]
+              src="https://images.alphacoders.com/128/thumb-1920-1282982.jpg"
+              alt="card_image"
+              data-aos="fade-up"
+              width="1000"
+              height="1000"
+            ></img>
+            <br></br>
+            <br></br>
+            <h1
+              className=" text-3xl font-bold text-black"
+              data-aos="fade-down"
+            >
+              Valorant&nbsp;{Topups} คน
+            </h1>
+          </div>
+        </div>
+      </div>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
 
       <div className="snowflake">❅</div>
       <div className="snowflake">❅</div>
