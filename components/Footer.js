09252d88629854307f32e379736c23956850c34a
Tanny <69077943+Paphawin130@users.noreply.github.com>
2023-11-15 21:03:26 +0700

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,10 @@
 export const FooterComponent = () => {
     return (
         <>
-            <div className="">
-                <footer className="page-footer text-black font-small blue pt-4 text-light">
-                    <div className="footer-copyright text-center py-3">Made with ❤️ by Tanny Powered by Coding Club
+            <div className="mx-auto container flex flex-col sm:flex-row justify-between py-4 px-4 gap-1 sm:gap-0 text-sm sm:text-base whitespace-nowrap 2xl:px-0">
+                <footer className="p-6 bg-white/5 text-sm sm:text-base flex flex-col md:flex-row md:justify-between text-black page-footer  font-small blue pt-4 text-light">
+                    <div className="footer-copyright text-center py-3">
+                        <p className="text-center sm:text-left text-black/100">Made with ❤️ Powered by Coding Club</p>
                     </div>
                 </footer>
             </div>
