b3f044f073581e2af322bb24791113e109895c8b
Tanny <69077943+Paphawin130@users.noreply.github.com>
2023-11-15 20:07:22 +0700

    Update vote.js

--- pages/vote.js
+++ pages/vote.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 export default function vote() {
   const handleVote = async (event) => {
@@ -87,50 +87,83 @@ export default function vote() {
     }
   };
   return (
-    <main className="container text-center min-h-screen text-purple-400 " data-aos="fade-up">
+    <main
+      className="container text-center min-h-screen text-purple-400 "
+      data-aos="fade-up"
+    >
       <div id="__next">
         <div
           className="grid pt-3 d-flex justify-content-center"
           data-aos="zoom-in"
         >
           <center>
-            <img 
+            <img
               className="img-fluid rounded-top-lg"
               src="https://www.kornpitack.ac.th/admin/index_page/file/1/0001.png"
               alt="banner"
               // width="2000"
               // height="2000"
             ></img>
-          </center> 
+          </center>
         </div>
         <br></br>
 
         <div className="bg-white p-4 rounded-md shadow-md">
-          <p className="text-center mb-2">เลือกเกมได้เลย !</p>
+          <p className="text-center text-2xl text-pink-500 mb-2">
+            เลือกเกมได้เลย !
+          </p>
 
-          <div
-            class="w-[400px] h-fit overflow-hidden rounded-md shadow-md transition duration-300 hover:cursor-pointer hover:-translate-y-1 hover:shadow-lg aos-init aos-animate"
-            data-aos="fade-up"
-            data-aos-delay="0"
-          >
-            <img
-              class="w-[400px] h-[188px] object-cover"
-              src="https://images.alphacoders.com/128/thumb-1920-1282982.jpg"
-              alt="card_image"
-              data-aos="fade-up"
-              // width="1000"
-              // height="1000"
-            ></img>
-            <Form onClick={handleVote}>
-            <div class="p-4 bg-gradient-to-tr from-purple-200 to-white">
-              <Button class="text-xl font-semibold" onClick={handleVote}> Vote</Button>
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 gap-x-4 text-black-500 justify-center text-center pt-7 px-5">
+            <div
+              className="relative rounded-xl shadow-xl bg-white border-white-500 transition ease-in-out delay-50 hover:-translate-y-7 hover:scale-11 duration-300 cursor-pointer"
+              data-aos="fade-left"
+              data-aos-duration="800"
+            >
+              <Form onClick={handleVote}>
+                <img
+                  class="img-fluid rounded-top-lg object-cover w-full h-full" // w-[400px] h-[200px]
+                  src="https://images.alphacoders.com/128/thumb-1920-1282982.jpg"
+                  alt="card_image"
+                  data-aos="fade-up"
+                  // width="1000"
+                  // height="1000"
+                ></img>
+
+                <div className="absolute bottom-5 left-10 flex flex-col items-start">
+                  <h1 className="text-red-500 text-4xl">Valorant</h1>
+                  <p className="text-red-50">เกม fps 5v5 บลาบลาบลาบลา</p>
+                </div>
+                <h1 className="text-white text-4xl absolute bottom-10 right-10">
+                  ระบบยังไม่เปิด Vote
+                </h1>
+              </Form>
             </div>
-            </Form>
+            <div
+              className="relative rounded-xl shadow-xl bg-white border-white-500 transition ease-in-out delay-50 hover:-translate-y-7 hover:scale-11 duration-300 cursor-pointer"
+              data-aos="fade-left"
+              data-aos-duration="800"
+            >
+              <Form onClick={handleVote}>
+                <img
+                  class="img-fluid rounded-top-lg object-cover w-full " // w-[400px] h-[200px]
+                  src="https://press-start.com.au/wp-content/uploads/2022/09/OW2-Header-770x433.jpg"
+                  alt="card_image"
+                  data-aos="fade-up"
+                  width="1000"
+                  height="1000"
+                ></img>
+                <div className="absolute bottom-5 left-10 flex flex-col items-start">
+                  <h1 className="text-red-500 text-4xl">Overwatch 2</h1>
+                  <p className="text-red-50">เกม fps 5v5 บลาบลาบลาบลา</p>
+                </div>
+                <h1 className="text-white text-4xl absolute bottom-10 right-10">
+                  ระบบยังไม่เปิด Vote
+                </h1>
+              </Form>
+            </div>
           </div>
         </div>
       </div>
-
-     
     </main>
   );
 }
